32bad55e60ab2846180cf72f2a552755ab1c1166
syalsr <emil07102000@gmail.com>
2022-05-31 18:36:09 +0300

    yet another commit

--- SimpleTemplateAllocator.hpp
+++ SimpleTemplateAllocator.hpp
@@ -7,6 +7,10 @@
 template<typename T>
 struct Allocator
 {
+    using value_type    = T;
+    using pointer       = T*;
+    using const_pointer = const T*;
+
     T* allocate(std::size_t count)
     {
         auto p = malloc(count * sizeof(T));
@@ -14,7 +18,7 @@ struct Allocator
             throw std::bad_alloc{};
         return reinterpret_cast<T*>(p);
     }
-    void deallocate(T* p)
+    void deallocate(T* p, std::size_t count)
     {
         free(p);
     }
